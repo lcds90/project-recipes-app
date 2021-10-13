eea6c0523d6bfaee07b5acc245b96b4a289e75dc
Leonardo Santos <lcds90@gmail.com>
2021-10-13 14:59:50 -0300

    perfil css done

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { useHistory } from 'react-router';
 import { Button, Header, Footer } from '../../components';
 import Context from '../../context/Context';
+import style from './Profile.module.css';
 
 const Profile = () => {
   const {
@@ -11,28 +12,30 @@ const Profile = () => {
   return (
     <>
       <Header title="Perfil" displaySearchBtn={ false } />
-      <p data-testid="profile-email">{email}</p>
-      <Button
-        dataTestId="profile-done-btn"
-        onClick={ () => history.push('/receitas-feitas') }
-      >
-        Receitas Feitas
-      </Button>
-      <Button
-        dataTestId="profile-favorite-btn"
-        onClick={ () => history.push('/receitas-favoritas') }
-      >
-        Receitas Favoritas
-      </Button>
-      <Button
-        dataTestId="profile-logout-btn"
-        onClick={ () => {
-          localStorage.clear();
-          history.push('/');
-        } }
-      >
-        Sair
-      </Button>
+      <section className={ style.section }>
+        <p data-testid="profile-email">{email}</p>
+        <Button
+          dataTestId="profile-done-btn"
+          onClick={ () => history.push('/receitas-feitas') }
+        >
+          Receitas Feitas
+        </Button>
+        <Button
+          dataTestId="profile-favorite-btn"
+          onClick={ () => history.push('/receitas-favoritas') }
+        >
+          Receitas Favoritas
+        </Button>
+        <Button
+          dataTestId="profile-logout-btn"
+          onClick={ () => {
+            localStorage.clear();
+            history.push('/');
+          } }
+        >
+          Sair
+        </Button>
+      </section>
       <Footer />
     </>
   );
